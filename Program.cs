92a9ed83c9c9ec5a40654830863bb1cd9b3de51b
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 08:06:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Luvut välillä 1-99, jotka ovat jaollisia kolmella:");
+
+        for (int i = 1; i <= 99; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
